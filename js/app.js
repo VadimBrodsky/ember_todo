7e54a77581fc51c1d2b3039a1d8c182572385213
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-02-20 23:38:32 -0500

    Add fixtures

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 // Create a new instance of `Ember.Application`
 // Make it available as a variable named `Todos` within your browser's JavaScript environment.
 window.Todos = Ember.Application.create();
+
+// Adapters are responsible for communicating with a source of data for your application.
+// `ApplicationAdapter` is an extension of the `DS.FixtureAdapter`.
+// Here we are using an adapter designed to load fixture data.
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
--- js/models/todo.js
+++ js/models/todo.js
@@ -4,3 +4,22 @@ Todos.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
+
+//Fixtures -  a way to put sample data into an application before connecting to long-term persistence
+Todos.Todo.FIXTURES = [
+ {
+   id: 1,
+   title: 'Learn Ember.js',
+   isCompleted: true
+ },
+ {
+   id: 2,
+   title: '...',
+   isCompleted: false
+ },
+ {
+   id: 3,
+   title: 'Profit!',
+   isCompleted: false
+ }
+];
