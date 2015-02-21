b69c4635f8ccdf9063b1489c56b7a878e3c713cd
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-02-20 23:30:18 -0500

    Initialize the app

---
+++ js/app.js
@@ -0,0 +1,3 @@
+// Create a new instance of `Ember.Application`
+// Make it available as a variable named `Todos` within your browser's JavaScript environment.
+window.Todos = Ember.Application.create();
