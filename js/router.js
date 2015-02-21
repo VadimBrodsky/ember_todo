7cace04f7e33ad2ff12ccdaeda5e3d52d98b4e62
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-02-20 23:46:51 -0500

    Display model data

--- js/router.js
+++ js/router.js
@@ -2,3 +2,13 @@ Todos.Router.map(function() {
   // Ember.js will detect when the application's URL matches '/' and to render the `todos` template.
   this.resource('todos', { path: '/' });
 });
+
+
+// Custom behavior (returning a specific set of models), 
+// Implement the class and add the desired behavior.
+// Ember.js has created a controller for us and set the controller's model property to our route's model.
+Todos.TodosRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('todo');
+  }
+});
